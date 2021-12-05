125f1b146f99be402c3310b7f3f0765e3e946e1c
Manish <manishdevrani777@gmail.com>
2021-12-05 22:19:32 +0530

    feat-added settings btn functionality

--- js/script.js
+++ js/script.js
@@ -4,20 +4,16 @@ const startTimerButton = document.querySelector('.start-timer');
 const settings = document.querySelector('.settings');
 const clockCircle = document.querySelector('.circle');
 
-startTimerButton.addEventListener('click', beginTimer);
 
+startTimerButton.addEventListener('click', startPomodoroTimer);
+settings.addEventListener('click', updatePomodoroSettings);
 
 let myTimer;
 
-function beginTimer(e) {
-    let inputMinutes = Number(minutes.firstElementChild.value);
-    let inputSeconds = Number(seconds.firstElementChild.value);
-    let totalTime = inputMinutes * 60 + inputSeconds;
-    startTimerButton.innerText = 'STOP';
-
-    if(e.target.innerText === 'STOP') {
-        stopTimer();
-    }
+function startPomodoroTimer(e) {
+    let mins = Number(minutes.firstElementChild.value);
+    let sec = Number(seconds.firstElementChild.value);
+    let totalTime = (mins * 60) + sec;
 
     function timer() {
         let m = Math.floor(totalTime / 60);
@@ -26,14 +22,40 @@ function beginTimer(e) {
         seconds.firstElementChild.value = s;
         totalTime--;
     }
-    
-    if(myTimer === undefined) {
-        myTimer = setInterval(timer, 1000);
+
+    if(e.target.innerText === 'STOP') {
+        stopTimer();
+        startTimerButton.innerText = 'START';
     } else {
-        console.log('already working!')
+        startTimerButton.innerText = 'STOP';
+        if(myTimer === undefined) {
+            myTimer = setInterval(timer, 1000);
+        } else {
+            console.log('already working!')
+        }
     }
 }
 
 function stopTimer() {
     clearInterval(myTimer);
+    myTimer = undefined;
+}
+
+
+function updatePomodoroSettings(e) {
+    let data, src, disabled;
+    if(e.target.dataset.work === 'settings') {
+        data = 'check';
+        src = '/STARTER/images/check.svg';
+        disabled = false;
+    } else {
+        data = 'settings';
+        src = '/STARTER/images/gear.svg';
+        disabled = true;
+    }
+
+    e.target.dataset.work = data;
+    settings.src = src;
+    minutes.firstElementChild.disabled = disabled;
+    seconds.firstElementChild.disabled = disabled;
 }
